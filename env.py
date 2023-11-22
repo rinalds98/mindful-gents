f62fc3b18339f0edb1e6fff8bbe737889c9eb00c
rz <rinalds98@gmail.com>
2023-11-22 10:33:15 +0000

    remove env.py

--- env.py
+++ env.py
@@ -1,4 +1,4 @@
 import os
 
-os.environ("SECRET_KEY", "1ozm)xek=1alb-ajjqw&z^1ldftkpi)c$173v5=k0&pl8tmmr$")
-os.environ("DATABASE_URL", "postgres://glmfjwkw:i0wTt6zFTPvASTeW-NYHIIWcbL8QNQiL@trumpet.db.elephantsql.com/glmfjwkw")
+os.environ["SECRET_KEY"] = "h-$0n148!n)93#w96^ipm5$5vccy)+q9x%t1n5gcjdk7p+cah2"
+os.environ["DATABASE_URL"] = "postgres://umjawvej:fJRslh1-y2Y4s-swy_r0KyH9vKqrasHD@flora.db.elephantsql.com/umjawvej"

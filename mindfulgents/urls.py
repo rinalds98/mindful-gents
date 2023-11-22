903ccb14feffc4289b69b7ce20f1466536740f92
rz <rinalds98@gmail.com>
2023-11-22 16:47:06 +0000

    Add allauth and account templates

--- mindfulgents/urls.py
+++ mindfulgents/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include, re_path
 from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.index, name='index'),
+    re_path(r'^accounts/', include('allauth.urls')), 
 ]

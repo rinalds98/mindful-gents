21fd7849d692d34727a04567a34adefceec9cccc
koko <k.cisek@gmail.com>
2023-11-22 23:30:34 +0000

    Update asgi config to initialize Django ASGI application early.

--- mindfulgents/asgi.py
+++ mindfulgents/asgi.py
@@ -7,17 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
-import os
 from django.core.asgi import get_asgi_application
+import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chat import routing
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mindfulgents.settings")
-
-
-# application = get_asgi_application()
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+application = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {

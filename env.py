d579dc5b819531c8a8870005994f1e0f87e5833c
rz <rinalds98@gmail.com>
2023-11-22 10:34:19 +0000

    remove env.py

--- env.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.environ["SECRET_KEY"] = "h-$0n148!n)93#w96^ipm5$5vccy)+q9x%t1n5gcjdk7p+cah2"
-os.environ["DATABASE_URL"] = "postgres://umjawvej:fJRslh1-y2Y4s-swy_r0KyH9vKqrasHD@flora.db.elephantsql.com/umjawvej"

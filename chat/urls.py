09db1ce1f5708cf394463e9714c810b72d8dd635
koko <k.cisek@gmail.com>
2023-11-24 10:16:46 +0000

    Add 'room' to chat romm url.

--- chat/urls.py
+++ chat/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = "chat"
 
 urlpatterns = [
-    path("<str:room_name>/", views.chat_room, name="chat"),
-     path("", views.chat_page, name="index"),
+    path("room/<str:room_name>/", views.chat_room, name="chat"),
+    path("", views.chat_page, name="index"),
 ]
- 

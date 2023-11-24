3f3a721c51a5fe4e0b939490e85d2cd173d4bafb
koko <k.cisek@gmail.com>
2023-11-24 16:47:08 +0000

    Update to prevent access to a room by pasting link.

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, Http404
 from chat.models import OpenRoom
 from django.contrib.auth.decorators import login_required
 
@@ -8,8 +8,16 @@ def chat_page(request):
     return render(request, "chatpage.html")
 
 
-def chat_room(request, room_name, *args, **kwargs):
+def get_referer(request):
+    referer = request.META.get('HTTP_REFERER')
+    if not referer:
+        return None
+    return referer
+
+
+def chat_room(request, room_name):
     """Render chat room page"""
+
     anonym_name = None
     if not request.user.is_authenticated:
         anonym_name = room_name
@@ -18,9 +26,13 @@ def chat_room(request, room_name, *args, **kwargs):
     current_room, created = OpenRoom.objects.get_or_create(
         chat_room_name=room_name, chat_room_url=request.path)
 
+    if not get_referer(request):
+        raise Http404
+
     context = {"anonym_name": anonym_name,
                "room_name": room_name,
                "current_room": current_room}
+
     return render(request, "chatroom.html", context)
 
 
@@ -37,8 +49,8 @@ def chat_lobby(request):
     return render(request, "chatlobby.html", context)
 
 
-def leave_room(request, pk):
+def leave_room(pk):
     room = OpenRoom.objects.get(id=pk)
-    print(room)
+
     room.delete()
     return redirect('index')

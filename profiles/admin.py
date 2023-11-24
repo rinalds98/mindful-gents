b98e7d76d066a7f09c0f902fe5420eec7487e5f7
rz <rinalds98@gmail.com>
2023-11-24 21:33:52 +0000

    update user registration process

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import UserProfile
 
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'isExpert', 'expertinfo', 'description')
 
-list_display = ('user', 'isExpert')
-admin.site.register(UserProfile)
+admin.site.register(UserProfile, UserProfileAdmin)

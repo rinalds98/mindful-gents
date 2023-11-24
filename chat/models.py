cdd8748137e3f600dadce2610906c1bb3c78d08c
koko <k.cisek@gmail.com>
2023-11-24 22:55:10 +0000

    Update room models.

--- chat/models.py
+++ chat/models.py
@@ -3,4 +3,5 @@ from django.db import models
 # Create your models here.
 class OpenRoom(models.Model):
     chat_room_name = models.CharField(max_length=50)
-    chat_room_url = models.URLField()
+    chat_room_url = models.CharField(max_length=200)
+    

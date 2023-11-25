7ef222066a8df57a0939d46257a87dcbf012377a
rz <rinalds98@gmail.com>
2023-11-25 14:02:27 +0000

    test2

--- chat/apps.py
+++ chat/apps.py
@@ -1,32 +1,5 @@
 from django.apps import AppConfig
-import asyncio
-import signal
-import os
-
-import websockets
 
 class ChatConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'chat'
-
-async def echo(websocket):
-    async for message in websocket:
-        await websocket.send(message)
-
-
-async def main():
-    # Set the stop condition when receiving SIGTERM.
-    loop = asyncio.get_running_loop()
-    stop = loop.create_future()
-    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
-
-    async with websockets.serve(
-        echo,
-        host="",
-        port=int(os.environ["PORT"]),
-    ):
-        await stop
-
-
-if __name__ == "__main__":
-    asyncio.run(main())

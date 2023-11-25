6ec89f6bc84a3a33f275c581a96be0320c4661eb
Ant <ant.griffiths22@gmail.com>
2023-11-25 14:28:41 +0000

    Styled chat

--- profiles/forms.py
+++ profiles/forms.py
@@ -6,8 +6,22 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ['isExpert', 'expertinfo', 'description']
+        labels = {
+            'isExpert': 'Please confirm you are an expert/professional?',
+            'expertinfo': 'What qualifies you as an expert/professional?',
+            'description': 'Please describe any other relevant experience you have and list any qualifications:',
+        }
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 4}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].required = True
+
 
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'email'] 
+        fields = ['username', 'email']

c4d8ac8b5a21bdc3474a67999ff74d855b375712
Koko-66 <52763151+Koko-66@users.noreply.github.com>
2023-11-25 09:35:35 +0000

    Chat - updates to the models and room objects logic (#60)
    * Update room models.
    
    * Add refresh script to lobby page.
    
    * Handle input with spaces and add base styling in chatroom.html.
    
    * Fix link.
    
    * Update functions for room and lobby.
    
    Add proofing for duplicate room names. Add different redirects for different user types.
    
    * Move OpenRoom creation to consumers and add deletion.
    
    Move function to update name with suffix to JavaScript in chatpage.html.

--- chat/urls.py
+++ chat/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("room/<str:room_name>/", views.chat_room, name="chat"),
     path("", views.chat_page, name="index"),
     path("lobby/", views.chat_lobby, name="lobby"),
-    path("leave_room/<int:pk>", views.leave_room, name="leave_room"),
+    path("leave_room/<str:room_name>", views.leave_room, name="leave_room"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, Http404
 from chat.models import OpenRoom
 from django.contrib.auth.decorators import login_required
+from random import randint
 
 
 def chat_page(request):
@@ -20,18 +21,14 @@ def chat_room(request, room_name):
 
     anonym_name = None
     if not request.user.is_authenticated:
-        anonym_name = room_name
+        anonym_name = room_name[:-4]
+        
 
-    # create a new OpenRoom object
-    current_room, created = OpenRoom.objects.get_or_create(
-        chat_room_name=room_name, chat_room_url=request.path)
-
-    if not get_referer(request):
-        raise Http404
-
-    context = {"anonym_name": anonym_name,
-               "room_name": room_name,
-               "current_room": current_room}
+    context = {
+        "anonym_name": anonym_name,
+        "room_name": room_name,
+        # "current_room": current_room,
+        }
 
     return render(request, "chatroom.html", context)
 
@@ -40,17 +37,18 @@ def chat_room(request, room_name):
 def chat_lobby(request):
     open_rooms = OpenRoom.objects.all()
 
-    url_base = request.get_host()
-    print("path:", url_base)
-
     context = {
-        "open_rooms":  open_rooms,
+        "open_rooms":  open_rooms,    
     }
+    
     return render(request, "chatlobby.html", context)
 
 
-def leave_room(request, pk):
-    room = OpenRoom.objects.get(id=pk)
+def leave_room(request, room_name):
+    room = OpenRoom.objects.get(chat_room_name=room_name)
 
     room.delete()
-    return redirect('index')
+    if not request.user.is_authenticated:
+        return redirect('index')
+    else:
+        return redirect("../lobby")
